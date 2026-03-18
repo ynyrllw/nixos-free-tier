d903b7d67c05917937c2a150196e3fb4f0d1ee20
Ynyr Llewelyn Williams <23356983+ynyrllw@users.noreply.github.com>
2026-03-18 07:43:34 +0000

    Add image capability schema with correct version 2024-03-27

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,6 +142,44 @@ resource "oci_core_shape_management" "nixos_a1_compat" {
   depends_on = [oci_core_image.nixos]
 }
 
+resource "oci_core_compute_image_capability_schema" "nixos_caps" {
+  compartment_id                      = var.compartment_ocid != "" ? var.compartment_ocid : var.tenancy_ocid
+  image_id                            = oci_core_image.nixos.id
+  compute_global_image_capability_schema_version_name = "2024-03-27"
+
+  schema_data = {
+    "Compute.Firmware" = jsonencode({
+      descriptorType = "enumstring"
+      source         = "IMAGE"
+      defaultValue   = "UEFI_64"
+      values         = ["UEFI_64"]
+    })
+
+    "Compute.LaunchMode" = jsonencode({
+      descriptorType = "enumstring"
+      source         = "IMAGE"
+      defaultValue   = "PARAVIRTUALIZED"
+      values         = ["PARAVIRTUALIZED", "EMULATED", "CUSTOM", "NATIVE"]
+    })
+
+    "Storage.BootVolumeType" = jsonencode({
+      descriptorType = "enumstring"
+      source         = "IMAGE"
+      defaultValue   = "PARAVIRTUALIZED"
+      values         = ["PARAVIRTUALIZED", "ISCSI", "SCSI", "IDE", "NVME"]
+    })
+
+    "Network.AttachmentType" = jsonencode({
+      descriptorType = "enumstring"
+      source         = "IMAGE"
+      defaultValue   = "PARAVIRTUALIZED"
+      values         = ["PARAVIRTUALIZED", "E1000", "VFIO", "VDPA"]
+    })
+  }
+
+  depends_on = [oci_core_image.nixos]
+}
+
 resource "oci_core_instance" "nixos" {
   compartment_id      = var.compartment_ocid
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
@@ -169,7 +207,8 @@ resource "oci_core_instance" "nixos" {
   }
 
   depends_on = [
-    oci_core_shape_management.nixos_a1_compat
+    oci_core_shape_management.nixos_a1_compat,
+    oci_core_compute_image_capability_schema.nixos_caps
   ]
 
   metadata = {
