c234b35a6b8ec10f2ed092fc00e3b31c018b25b4
Ynyr Llewelyn Williams <23356983+ynyrllw@users.noreply.github.com>
2026-03-19 10:33:55 +0000

    Fix: add OCI CLI config, use us-east-1 as S3 region

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,12 @@ terraform {
   }
   
   backend "s3" {
-    endpoint         = "https://objectstorage.eu-zurich-1.oraclecloud.com"
-    region           = "eu-zurich-1"
-    bucket           = "nixos-terraform-state"
-    key              = "terraform.tfstate"
-    access_key       = "nixos-terraform-state"
-    secret_key       = "nixos-terraform-state"
+    endpoint                    = "https://objectstorage.eu-zurich-1.oraclecloud.com"
+    region                      = "us-east-1"
+    bucket                      = "nixos-terraform-state"
+    key                         = "terraform.tfstate"
+    access_key                  = "nixos-terraform-state"
+    secret_key                  = "nixos-terraform-state"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
