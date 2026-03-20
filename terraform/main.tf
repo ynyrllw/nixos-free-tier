003f1293aec3c3dfb4c5d401eb3b02522ca9997b
Ynyr Llewelyn Williams <23356983+ynyrllw@users.noreply.github.com>
2026-03-20 08:20:57 +0000

    Fix Ubuntu image query for ARM64

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,10 +24,14 @@ data "oci_core_images" "ubuntu" {
   compartment_id           = var.tenancy_ocid
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
-  shape                    = "VM.Standard.A1.Flex"
 
   sort_by    = "TIMECREATED"
   sort_order = "DESC"
+
+  filter {
+    name   = "display_name"
+    values = [".*aarch64.*"]
+  }
 
   filter {
     name   = "limit"
