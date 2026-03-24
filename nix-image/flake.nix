7ae7caf0f243a40105645ef57160286878ed2f60
Ynyr Llewelyn Williams <23356983+ynyrllw@users.noreply.github.com>
2026-03-24 15:43:17 +0000

    Fix nixos-anywhere flake config

--- nix-image/flake.nix
+++ nix-image/flake.nix
@@ -3,14 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, disko }: {
     nixosConfigurations = {
       aarch64-linux = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
+          disko.nixosModules.disko
           ./configuration.nix
+          {
+            disko.device = "/dev/sda";
+          }
         ];
       };
     };
